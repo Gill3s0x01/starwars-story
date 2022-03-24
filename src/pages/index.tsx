bc79d378195a64c1d08a6a6faff6082f8f27ba91
Gilles30 <lorison.gilles@gmail.com>
2022-03-23 23:46:57 -0300

    🎨 Created styled main

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,32 @@
 import Head from 'next/head'
+import { SubscribeButton } from '../components/SubscribeButton'
+import styles from './home.module.scss'
+import Image from 'next/image'
 
 export default function Home() {
   return (
     <>
       <Head>StarWarsApp</Head>
-      <h1>teste</h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span> 👋 Hey, welcome </span>
+          <h1>
+            New about the
+            <span> Stars Wars</span> world.
+          </h1>
+          <p>
+            Get acesse to all the informations <br /> <span>$9.90</span>
+          </p>
+          <SubscribeButton />
+        </section>
+        <Image
+          className={styles.AvatarMain}
+          src="/images/avatar.png"
+          alt="Girl coding"
+          height={'520px'}
+          width={'334px'}
+        />
+      </main>
     </>
   )
 }
